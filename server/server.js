eeee66a8524ffe43b67327892e7a056b7be94c9a
unknown <varun.kamarapu@gmail.com>
2018-12-30 21:24:40 +0000

    Add GET todos Id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./model/Todo');
@@ -27,5 +28,18 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
   Todo.find().then((todos)=>{res.send({todos});}, (err)=>{res.status(400).send(err);});
 });
+
+app.get('/todos/:id', (req, res) => {
+  if(!ObjectID.isValid(req.params.id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(req.params.id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo})}, (err) =>{res.status(400).send(err);});
+
+})
 
 module.exports.app ={app}
