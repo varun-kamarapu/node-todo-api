200f819ee6a154cf225af82881b9200e28c4c80f
unknown <varun.kamarapu@gmail.com>
2018-12-31 18:25:24 +0000

    Added Delete/todos/:id route

--- server/server.js
+++ server/server.js
@@ -44,4 +44,16 @@ app.get('/todos/:id', (req, res) => {
 
 })
 
+app.delete('/todos/:id', (req, res) => {
+  if(!ObjectID.isValid(req.params.id)){
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(req.params.id).then((todo) =>{
+      if(!todo){
+        res.status(404).send();
+      }
+      res.send(todo);
+  }, (err) => {res.status(404).send(err)});
+});
+
 module.exports.app ={app}
